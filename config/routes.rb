5a1c8fe338aa4f40cc6b86e5c3b35001102c42b4
bryanbenjamin <bryan@battung.org>
2014-10-22 23:39:26 +0400

    edited pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'My_App/home'
+  get 'pages/home'
 
-  get 'My_App/contact'
+  get 'pages/contact'
 
 
   # The priority is based upon order of creation: first created -> highest priority.

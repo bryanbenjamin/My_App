0acf9d30c2fc2a190c8b49c7bea04e9bc6856349
bryanbenjamin <bryan@battung.org>
2014-10-23 23:01:46 +0400

    End of chapter 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages#home'
-
-  get 'pages/contact'
+  root 'pages#home'
   
   get 'pages/about'
-
+  
+  get 'pages/contact'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
